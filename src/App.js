6cfd6e3a952f468a26f806df585a62d479b6d8a1
cintya <rmishel564@gmail.com>
2023-03-24 11:05:45 -0500

    se realiza estructura base del proyecto1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="projectone">
+        <div className="masterbox">
+          <h1>Join our community</h1>
+          <h2>30-day hassle free money back guarante</h2>
+          <p> gain access to uor full library of tutorials along with expert code reviews. perfect for any developers who are serious about their skills </p>
+        </div>
+
+        <div>
+          <h2>Monthly Subcription</h2>
+          <p>$29</p>
+          <p>per month</p>
+          <p>full access for less than $1 a day </p>
+          <button>Sing Up</button>
+        </div>
+
+        <div>
+          <h2>Why Us</h2>
+          <p>tutorials by industry experts peer & expert code review coding exercises Access touor gitHub repos Community forum flashcard decks New videos every week</p>
+        </div>
+      </div>
     </div>
   );
 }
