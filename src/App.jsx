bdfbdd3f3654f42413fa1c6c0442e754afab8d64
cintya <rmishel564@gmail.com>
2023-03-27 12:01:31 -0500

    se cambia modifica proyecto base y se añaden estilos

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Boxmaster from "./Component/Boxmaster"
+
+
+function App() {
+  return (
+    <>
+    <div className='container'>
+    <div className="container2">
+      <div className="box">
+      <Boxmaster/>
+      </div>
+      </div>
+    </div>
+       </>
+  );
+}
+
+export default App;
---
+++ src/Component/Boxmaster.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+
+      <div className="masterbox">
+
+        <h1 className="titulo">Join our community</h1>
+        <h2 className="titulo2">30-day hassle free money back guarante</h2>
+        <p className="parrafo1"> gain access to uor full library of tutorials along with expert code reviews. perfect for any developers who are serious about their skills </p>
+      </div>
+      <div className="container3">
+        <div className="box2">
+          <h2>Monthly Subcription</h2>
+          <p>$29</p>
+          <p>per month</p>
+          <p>full access for less than $1 a day </p>
+          <button>Sing Up</button>
+        </div>
+
+        <div className="box3">
+          <h2>Why Us</h2>
+          <p>tutorials by industry experts peer & expert code review coding exercises Access touor gitHub repos Community forum flashcard decks New videos every week</p>
+        </div>
+      </div>
+    </>
+  );
+}
