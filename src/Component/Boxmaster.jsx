1acdfa8f48119c5252b2567ed4e8bda608c45724
cintya <rmishel564@gmail.com>
2023-03-27 19:19:46 -0500

    se añade tipo de fuente y tamano a la tipografia

--- src/Component/Boxmaster.jsx
+++ src/Component/Boxmaster.jsx
@@ -7,21 +7,21 @@ export default function Footer() {
       <div className="masterbox">
 
         <h1 className="titulo">Join our community</h1>
-        <h2 className="titulo2">30-day hassle free money back guarante</h2>
-        <p className="parrafo1"> gain access to uor full library of tutorials along with expert code reviews. perfect for any developers who are serious about their skills </p>
+        <h2 className="titulo1">30-day, hassle free money back guarante</h2>
+        <p className="parrafo1"> gain access to uor full library of tutorials along with expert code reviews.<br/>perfect for any developers who are serious about their skills </p>
       </div>
       <div className="container3">
         <div className="box2">
-          <h2>Monthly Subcription</h2>
-          <p>$29</p>
-          <p>per month</p>
-          <p>full access for less than $1 a day </p>
-          <button>Sing Up</button>
+
+          <h2 className="titulo2">Monthly Subcription</h2>
+          <p className="numero"><b className="num">$29</b>   per month</p>
+          <p className="texto">full access for less than $1 a day </p>
+          <button className="boton">Sing Up</button>
         </div>
 
         <div className="box3">
-          <h2>Why Us</h2>
-          <p>tutorials by industry experts peer & expert code review coding exercises Access touor gitHub repos Community forum flashcard decks New videos every week</p>
+          <h2 className="titulo2">Why Us</h2>
+          <p className="texto2">tutorials by industry experts <br/> peer & expert code review<br/> coding exercises <br/>Access touor gitHub repos <br/>Community forum<br/> flashcard decks <br/>New videos every week</p>
         </div>
       </div>
     </>
